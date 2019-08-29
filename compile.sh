5bf8bb7feadf1eaf777c3ba79e1f20f9528d3ef8
Oliver Friedmann <oliver.friedmann@gmail.com>
2019-08-29 12:19:32 -0400

    ECS Fargate Task + Schedule

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 cd src/ecs-task-concurrency-limit ; cat main.yaml | ufpp > ../../dist/ecs-task-concurrency-limit.yaml ; cd ../..
+cd src/ecs-fargate-task ; cat main.yaml | ufpp > ../../dist/ecs-fargate-task.yaml ; cd ../..
+cd src/ecs-fargate-schedule ; cat main.yaml | ufpp > ../../dist/ecs-fargate-schedule.yaml ; cd ../..
